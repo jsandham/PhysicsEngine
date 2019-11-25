83d7baa2e83459345efe6d411e4455971348241e
James <jsandham@uwaterloo.ca>
2019-11-24 18:24:07 -0600

    organizing folders and enabling release build of engine and updating licenses

--- engine/lib/dp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-if not defined DevEnvDir (
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-	rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat" x64
-)
-
-rem dumpbin /SYMBOLS "C:\Users\jsand\Documents\PhysicsEngine\engine\lib\obj\Color.obj" > "dumpbin.txt"
-dumpbin /DISASM "C:\Users\jsand\Documents\PhysicsEngine\engine\lib\obj\Color.obj" > "dumpbin.txt"
